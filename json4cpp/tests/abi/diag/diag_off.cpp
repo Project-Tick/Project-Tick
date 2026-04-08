e2488c9a05d2fbc1009d6f919bff2663b88387d6
Mehmet Samet Duman <yongdohyun@projecttick.org>
2026-04-08 19:49:14 +0300

    NOISSUE Update JSON for Modern C++ to version 10.0.3 and change copyright information
    - Updated version number from 3.12.0 to 10.0.3 in all test files.
    - Changed copyright holder from Niels Lohmann to Project Tick in all relevant files.
    - Ensured all files reflect the new project repository link: https://github.com/Project-Tick/Project-Tick.
    
    Signed-off-by: Mehmet Samet Duman <yongdohyun@projecttick.org>

--- json4cpp/tests/abi/diag/diag_off.cpp
+++ json4cpp/tests/abi/diag/diag_off.cpp
@@ -1,9 +1,10 @@
-//     __ _____ _____ _____
-//  __|  |   __|     |   | |  JSON for Modern C++ (supporting code)
-// |  |  |__   |  |  | | | |  version 3.12.0
-// |_____|_____|_____|_|___|  https://github.com/nlohmann/json
+//     __ _____ _____ _____   _     _   
+//  __|  |   __|     |   | |_| |_ _| |_   JSON for Modern C++ (supporting code)
+// |  |  |__   |  |  | | | |_   _|_   _|  version 10.0.3
+// |_____|_____|_____|_|___| |_|   |_|    https://github.com/Project-Tick/Project-Tick
 //
 // SPDX-FileCopyrightText: 2013-2026 Niels Lohmann <https://nlohmann.me>
+// SPDX-FileCopyrightText: 2026 Project Tick
 // SPDX-License-Identifier: MIT
 
 #undef JSON_DIAGNOSTICS
--- json4cpp/tests/src/unit-alt-string.cpp
+++ json4cpp/tests/src/unit-alt-string.cpp
@@ -1,10 +1,11 @@
-//     __ _____ _____ _____
-//  __|  |   __|     |   | |  JSON for Modern C++ (supporting code)
-// |  |  |__   |  |  | | | |  version 3.12.0
-// |_____|_____|_____|_|___|  https://github.com/nlohmann/json
+//     __ _____ _____ _____   _     _   
+//  __|  |   __|     |   | |_| |_ _| |_   JSON for Modern C++ (supporting code)
+// |  |  |__   |  |  | | | |_   _|_   _|  version 10.0.3
+// |_____|_____|_____|_|___| |_|   |_|    https://github.com/Project-Tick/Project-Tick
 //
 // SPDX-FileCopyrightText: 2013-2026 Niels Lohmann <https://nlohmann.me>
 // SPDX-FileCopyrightText: 2018 Vitaliy Manushkin <agri@akamo.info>
+// SPDX-FileCopyrightText: 2026 Project Tick
 // SPDX-License-Identifier: MIT
 
 #include "doctest_compatibility.h"
