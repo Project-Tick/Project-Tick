2581d0084e746be0088b601d5457646f7e0e6563
Mehmet Samet Duman <yongdohyun@projecttick.org>
2026-04-10 16:08:11 +0300

    NOISSUE Update Git version and improve CMake integration for JSON fetching in tests
    Signed-off-by: Mehmet Samet Duman <yongdohyun@projecttick.org>

--- cgit/tests/t0001-validate-git-versions.sh
+++ cgit/tests/t0001-validate-git-versions.sh
@@ -12,17 +12,33 @@ test_expect_success 'extract Git version from Makefile' '
 	sed -n -e "/^GIT_VER[ 	]*=/ {
 		s/^GIT_VER[ 	]*=[ 	]*//
 		p
-	}" ../../Makefile >makefile_version
+	}" ../../Makefile >makefile_version &&
+	makefile_ver=$(cat makefile_version) &&
+	if (
+		cd ../../git &&
+		git rev-parse --verify -q "$makefile_ver^{commit}" >/dev/null
+	)
+	then
+		(
+			cd ../../git &&
+			git rev-parse "$makefile_ver^{commit}"
+		) >makefile_git_oid &&
+		(
+			cd ../../git &&
+			git describe --match "v[0-9]*" "$makefile_ver"
+		) | sed -e "s/^v//" -e "s/-/./g" >makefile_git_version
+	else
+		sed -e "s/-/./g" makefile_version >makefile_git_version
+	fi
 '
 
 # Note that Git's GIT-VERSION-GEN script applies "s/-/./g" to the version
-# string to produce the internal version in the GIT-VERSION-FILE, so we
-# must apply the same transformation to the version in the Makefile before
-# comparing them.
+# string to produce the internal version in the GIT-VERSION-FILE. The cgit
+# Makefile may carry either such a version string or an exact git commit-ish,
+# so normalize to the internal version form before comparing them.
 test_expect_success 'test Git version matches Makefile' '
 	( cat ../../git/GIT-VERSION-FILE || echo "No GIT-VERSION-FILE" ) |
 	sed -e "s/GIT_VERSION[ 	]*=[ 	]*//" -e "s/\\.dirty$//" >git_version &&
-	sed -e "s/-/./g" makefile_version >makefile_git_version &&
 	test_cmp git_version makefile_git_version
 '
 
@@ -31,14 +47,24 @@ test_expect_success 'test submodule version matches Makefile' '
 	then
 		echo "git/ is not a Git repository" >&2
 	else
-		(
-			cd ../.. &&
-			sm_oid=$(git ls-files --stage -- git |
-				sed -e "s/^[0-9]* \\([0-9a-f]*\\) [0-9]	.*$/\\1/") &&
-			cd git &&
-			git describe --match "v[0-9]*" $sm_oid
-		) | sed -e "s/^v//" -e "s/-/./" >sm_version &&
-		test_cmp sm_version makefile_version
+		if test -f makefile_git_oid
+		then
+			(
+				cd ../.. &&
+				git ls-files --stage -- git |
+				sed -n -e "s/^[0-9]* \\([0-9a-f]*\\) [0-9]	.*$/\\1/p"
+			) >sm_oid &&
+			test_cmp sm_oid makefile_git_oid
+		else
+			(
+				cd ../.. &&
+				sm_oid=$(git ls-files --stage -- git |
+					sed -e "s/^[0-9]* \\([0-9a-f]*\\) [0-9]	.*$/\\1/") &&
+				cd git &&
+				git describe --match "v[0-9]*" $sm_oid
+			) | sed -e "s/^v//" -e "s/-/./g" >sm_version &&
+			test_cmp sm_version makefile_version
+		fi
 	fi
 '
 
